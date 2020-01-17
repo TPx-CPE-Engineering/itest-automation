5d887df00dbba9282f48cac78f389dbf6c849e71
dataeng <cpe-itest@tpx.com>
2020-01-17 09:49:12 -0800

    Rename client and api variables
    Done to have more specific names since they are in the global namespace

--- d_velocloud/Login/operator_login.py
+++ d_velocloud/Login/operator_login.py
@@ -8,12 +8,12 @@ operator_login_username = 'juan.brena@tpx.com'
 operator_login_password = '1Maule1!'
 
 # Set client and api
-client = ApiClient(host=operator_login_host)
-api = AllApi(api_client=client)
+velocloud_client = ApiClient(host=operator_login_host)
+velocloud_api = AllApi(api_client=velocloud_client)
 
 # Make auth call
 try:
-    client.authenticate(username=operator_login_username, password=operator_login_password, operator=True)
+    velocloud_client.authenticate(username=operator_login_username, password=operator_login_password, operator=True)
 except ApiException as login_exception:
     print(login_exception)
     exit()
