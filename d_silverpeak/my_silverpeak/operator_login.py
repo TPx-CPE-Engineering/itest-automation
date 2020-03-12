3c07cb85fe59ad832a02cd05ed0af21f1e8384e9
dataeng <cpe-itest@tpx.com>
2020-03-12 13:34:53 -0700

    Wait on Velocloud to confirm if API calls are performed correctly

--- d_silverpeak/my_silverpeak/operator_login.py
+++ d_silverpeak/my_silverpeak/operator_login.py
@@ -3,4 +3,5 @@ from silverpeak import *
 
 def login():
     sp = Silverpeak(user='juan.brena', user_pass='1Maule1!', sp_server='cpesp.lab-sv.telepacific.com', disable_warnings=True, auto_login=True)
+    print(sp.login_result)
     return sp
