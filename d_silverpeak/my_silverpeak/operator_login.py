b855404ef6965e4c2f40c1e061e11b44ad3c6221
dataeng <cpe-itest@tpx.com>
2020-03-12 13:38:00 -0700

    Add silverpeak login error handle

--- d_silverpeak/my_silverpeak/operator_login.py
+++ d_silverpeak/my_silverpeak/operator_login.py
@@ -3,5 +3,9 @@ from silverpeak import *
 
 def login():
     sp = Silverpeak(user='juan.brena', user_pass='1Maule1!', sp_server='cpesp.lab-sv.telepacific.com', disable_warnings=True, auto_login=True)
-    print(sp.login_result)
+
+    if not sp.login_result.ok:
+        print(sp.login_result)
+        exit()
+
     return sp
