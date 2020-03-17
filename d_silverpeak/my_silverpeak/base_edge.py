c990ab069710bb663267f0f23cb1f529da442267
dataeng <cpe-itest@tpx.com>
2020-03-17 15:11:48 -0700

    Add a template for Silverpeak base edge

---
+++ d_silverpeak/my_silverpeak/base_edge.py
@@ -0,0 +1,5 @@
+class BaseEdge:
+    def __init__(self, edge_id, enterprise_id, ssh_port):
+        self.edge_id = edge_id
+        self.enterprise_id = enterprise_id
+        self.ssh_port = ssh_port
