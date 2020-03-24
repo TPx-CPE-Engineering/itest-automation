c7052a157d0cc2c6f5a84d9bf50a866ff890d79a
dataeng <cpe-itest@tpx.com>
2020-03-24 13:00:42 -0700

    Move api into Edge object, Clean print results, Update removal of firewall rule

--- d_silverpeak/Firewall/firewall_verify_outbound_destination_rules.py
+++ d_silverpeak/Firewall/firewall_verify_outbound_destination_rules.py
@@ -1,4 +1,3 @@
-from my_silverpeak import operator_login
 from my_silverpeak.base_edge import SPBaseEdge
 import json
 
@@ -9,23 +8,32 @@ ZB Firewall
 Author: juan.brena@tpx.com
 Date: 3/18/2020
 
-Test Case: ZB Firewall - Verify outbound IPaddr destination rules (NAT)
-Expectations: Configured traffic not allowed through firewall
-Usage: Configure outbound rule to deny all traffic based on destination address (Configuration > Templates > Default Template > Security Policies
-    > Click on Zone (One to Default) in Matrix > Add Rule > Priority 1500 > Match Criteria "Destination IP" > 
-    "4.2.2.2"; Verify unable to ping 4.2.2.2 from CPE)
+Test Case:      ZB Firewall - Verify outbound IPaddr destination rules (NAT)
+Expectations:   Configured traffic not allowed through firewall
+Usage:          Configure outbound rule to deny all traffic based on destination address (Configuration > Templates > Default Template > Security Policies >
+                Click on Zone (One to Default) in Matrix > Add Rule > Priority 1500 > Match Criteria "Destination IP" > "4.2.2.2"
+                Verify unable to ping 4.2.2.2 from CPE
 """
-
-# Login as Operator
-sp = operator_login.login()
 
 
 class SPEdge(SPBaseEdge):
     def __init__(self, edge_id, enterprise_id, ssh_port):
-        super().__init__(edge_id=edge_id, enterprise_id=enterprise_id, ssh_port=ssh_port)
+        super().__init__(edge_id=edge_id, enterprise_id=enterprise_id, ssh_port=ssh_port, auto_operator_login=True)
 
 
 EDGE: SPEdge
+
+
+def set_globals(edge_id: str, enterprise_id: str, ssh_port: str):
+    """
+    Creates Silver Peak EDGE object
+    :param edge_id: Silver Peak EDGE ID
+    :param enterprise_id: Not needed for Silver Peak but kept to reuse iTest test case
+    :param ssh_port: SSH Port for CPE sitting behind Silver Peak EDGE
+    :return: None
+    """
+    global EDGE
+    EDGE = SPEdge(edge_id=edge_id, enterprise_id=None, ssh_port=ssh_port)
 
 
 def add_firewall_outbound_rule_with_destination_ip(destination_ip):
@@ -55,7 +63,7 @@ def add_firewall_outbound_rule_with_destination_ip(destination_ip):
                                 }
 
     # Get EDGE's Security Policy Rules data
-    security_policy_rules = sp.get_sec_policy(applianceID=EDGE.edge_id).data
+    security_policy_rules = EDGE.api.get_sec_policy(applianceID=EDGE.edge_id).data
 
     # Add new rule to Security Policy Rules
     # Add to 12_0 since that is 'One to Default'
@@ -66,15 +74,13 @@ def add_firewall_outbound_rule_with_destination_ip(destination_ip):
     data = json.dumps(data)
 
     # Call API call
-    result = sp.post_sec_policy(applianceID=EDGE.edge_id, secPolData=data)
+    result = EDGE.api.post_sec_policy(applianceID=EDGE.edge_id, secPolData=data)
 
     # Check results
     if result.status_code == 204:
-        d = {'error': None, 'rows': 1}
-        print(d)
+        print({'error': None, 'rows': 1})
     else:
-        d = {'error': result.error, 'rows': 0}
-        print(d)
+        print({'error': result.error, 'rows': 0})
 
 
 def remove_firewall_outbound_rule_with_destination_ip(destination_ip):
@@ -85,13 +91,14 @@ def remove_firewall_outbound_rule_with_destination_ip(destination_ip):
     """
 
     # Get EDGE's Security Policy Rules data
-    security_policy_rules = sp.get_sec_policy(applianceID=EDGE.edge_id).data
+    security_policy_rules = EDGE.api.get_sec_policy(applianceID=EDGE.edge_id).data
 
     # Delete rule
     try:
         del security_policy_rules['map1']['12_0']['prio']['1500']
     except KeyError:
-        print("Deny destination address rule not found")
+        # If KeyError then rule does not exists therefore rule was removed
+        print({'error': None, 'rows': 0})
         return
 
     # Setup Data for API call
@@ -99,15 +106,13 @@ def remove_firewall_outbound_rule_with_destination_ip(destination_ip):
     data = json.dumps(data)
 
     # Call API call
-    result = sp.post_sec_policy(applianceID=EDGE.edge_id, secPolData=data)
+    result = EDGE.api.post_sec_policy(applianceID=EDGE.edge_id, secPolData=data)
 
     # Check results
     if result.status_code == 204:
-        d = {'error': None, 'rows': 1}
-        print(d)
+        print({'error': None, 'rows': 1})
     else:
-        d = {'error': result.error, 'rows': 0}
-        print(d)
+        print({'error': result.error, 'rows': 0})
 
 
 def is_firewall_outbound_rule_with_destination_ip_present(destination_ip):
@@ -117,7 +122,7 @@ def is_firewall_outbound_rule_with_destination_ip_present(destination_ip):
     :return: None
     """
     # Get EDGE's Security Policy Rules data
-    security_policy_rules = sp.get_sec_policy(applianceID=EDGE.edge_id).data
+    security_policy_rules = EDGE.api.get_sec_policy(applianceID=EDGE.edge_id).data
 
     # Attempt to get Zone base Firewall rule with priority 1500 on One to Default zone
     deny_source_address_rule = security_policy_rules.get('map1', None).get('12_0', None).get('prio', None).get('1500', None)
@@ -131,20 +136,5 @@ def is_firewall_outbound_rule_with_destination_ip_present(destination_ip):
         print({"is_firewall_outbound_rule_with_destination_ip_present": 'yes'})
 
 
-def set_globals(edge_id: str, enterprise_id: str, ssh_port: str):
-    """
-    Creates Silver Peak EDGE object
-    :param edge_id: Silver Peak EDGE ID
-    :param enterprise_id: Not needed for Silver Peak but kept to reuse iTest test case
-    :param ssh_port: SSH Port for CPE sitting behind Silver Peak EDGE
-    :return: None
-    """
-    global EDGE
-    EDGE = SPEdge(edge_id=edge_id, enterprise_id=None, ssh_port=ssh_port)
-
-
 if __name__ == '__main__':
     set_globals(edge_id='7.NE', enterprise_id='0', ssh_port="2201")
-    is_firewall_outbound_rule_with_destination_ip_present(destination_ip='1.1.1.1')
-    # add_firewall_outbound_rule_with_destination_ip(destination_ip='1.1.1.1')
-    remove_firewall_outbound_rule_with_destination_ip(destination_ip='1.1.1.1')
