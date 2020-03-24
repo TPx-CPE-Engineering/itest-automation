f83b3cc1f00aee6b374db2b741ee7c2363c4f259
dataeng <cpe-itest@tpx.com>
2020-03-24 13:11:53 -0700

    Move api into Edge object, Clean printing results

--- d_silverpeak/Firewall/firewall_verify_outbound_application_rules.py
+++ d_silverpeak/Firewall/firewall_verify_outbound_application_rules.py
@@ -1,4 +1,3 @@
-from my_silverpeak import operator_login
 from my_silverpeak.base_edge import SPBaseEdge
 import json
 
@@ -9,23 +8,33 @@ ZB Firewall
 Author: juan.brena@tpx.com
 Date: 3/18/2020
 
-Test Case: ZB Firewall - Verify outbound application destination rules (NAT)
-Expectations: Configured traffic not allowed through firewall
-Usage: Configure outbound rule to deny traffic based on Application  (Configuration > Templates > Default Template > 
-    Security Policies > Click on Zone (One to Default) in Matrix > Add Rule > Priority 1500 > 
-    Match Criteria "Application" > "ICMP"; Verify unable to register from CPE)
+Test Case:      ZB Firewall - Verify outbound application destination rules (NAT)
+Expectations:   Configured traffic not allowed through firewall
+Usage:          Configure outbound rule to deny traffic based on Application  (Configuration > Templates > Default Template > 
+                Security Policies > Click on Zone (One to Default) in Matrix > Add Rule > Priority 1500 > 
+                Match Criteria "Application" > "ICMP"
+                Verify unable to register from CPE)
 """
-
-# Login as Operator
-sp = operator_login.login()
 
 
 class SPEdge(SPBaseEdge):
     def __init__(self, edge_id, enterprise_id, ssh_port):
-        super().__init__(edge_id=edge_id, enterprise_id=enterprise_id, ssh_port=ssh_port)
+        super().__init__(edge_id=edge_id, enterprise_id=enterprise_id, ssh_port=ssh_port, auto_operator_login=True)
 
 
 EDGE: SPEdge
+
+
+def set_globals(edge_id: str, enterprise_id: str, ssh_port: str):
+    """
+    Creates Silver Peak EDGE object
+    :param edge_id: Silver Peak EDGE ID
+    :param enterprise_id: Not needed for Silver Peak but kept to reuse iTest test case
+    :param ssh_port: SSH Port for CPE sitting behind Silver Peak EDGE
+    :return: None
+    """
+    global EDGE
+    EDGE = SPEdge(edge_id=edge_id, enterprise_id=None, ssh_port=ssh_port)
 
 
 def add_icmp_block_outbound_app_rule():
@@ -50,7 +59,7 @@ def add_icmp_block_outbound_app_rule():
                                 }
 
     # Get EDGE's Security Policy Rules data
-    security_policy_rules = sp.get_sec_policy(applianceID=EDGE.edge_id).data
+    security_policy_rules = EDGE.api.get_sec_policy(applianceID=EDGE.edge_id).data
 
     # Add new rule to Security Policy Rules
     # Add to 12_0 since that is 'One to Default'
@@ -61,15 +70,13 @@ def add_icmp_block_outbound_app_rule():
     data = json.dumps(data)
 
     # Call API call
-    result = sp.post_sec_policy(applianceID=EDGE.edge_id, secPolData=data)
+    result = EDGE.api.post_sec_policy(applianceID=EDGE.edge_id, secPolData=data)
 
     # Check results
     if result.status_code == 204:
-        d = {'error': None, 'rows': 1}
-        print(d)
+        print({'error': None, 'rows': 1})
     else:
-        d = {'error': result.error, 'rows': 0}
-        print(d)
+        print({'error': result.error, 'rows': 0})
 
 
 def remove_icmp_block_outbound_app_rule():
@@ -79,13 +86,14 @@ def remove_icmp_block_outbound_app_rule():
     :return: None
     """
     # Get EDGE's Security Policy Rules data
-    security_policy_rules = sp.get_sec_policy(applianceID=EDGE.edge_id).data
+    security_policy_rules = EDGE.api.get_sec_policy(applianceID=EDGE.edge_id).data
 
     # Delete rule
     try:
         del security_policy_rules['map1']['12_0']['prio']['1500']
     except KeyError:
-        print("Deny application rule not found")
+        # If KeyError, rule does not exist therefore removal was sucessful
+        print({'error': None, 'rows': 0})
         return
 
     # Setup Data for API call
@@ -93,7 +101,7 @@ def remove_icmp_block_outbound_app_rule():
     data = json.dumps(data)
 
     # Call API call
-    result = sp.post_sec_policy(applianceID=EDGE.edge_id, secPolData=data)
+    result = EDGE.api.post_sec_policy(applianceID=EDGE.edge_id, secPolData=data)
 
     # Check results
     if result.status_code == 204:
@@ -111,7 +119,7 @@ def is_icmp_block_outbound_app_rule_present():
     :return: None
     """
     # Get EDGE's Security Policy Rules data
-    security_policy_rules = sp.get_sec_policy(applianceID=EDGE.edge_id).data
+    security_policy_rules = EDGE.api.get_sec_policy(applianceID=EDGE.edge_id).data
 
     # Attempt to get Zone base Firewall rule with priority 1500 on One to Default zone
     deny_source_address_rule = security_policy_rules.get('map1', None).get('12_0', None).get('prio', None).get('1500', None)
@@ -123,18 +131,6 @@ def is_icmp_block_outbound_app_rule_present():
     else:
         # If rule is not None, then rule exist
         print({"is icmp block outbound app rule present": 'yes'})
-
-
-def set_globals(edge_id: str, enterprise_id: str, ssh_port: str):
-    """
-    Creates Silver Peak EDGE object
-    :param edge_id: Silver Peak EDGE ID
-    :param enterprise_id: Not needed for Silver Peak but kept to reuse iTest test case
-    :param ssh_port: SSH Port for CPE sitting behind Silver Peak EDGE
-    :return: None
-    """
-    global EDGE
-    EDGE = SPEdge(edge_id=edge_id, enterprise_id=None, ssh_port=ssh_port)
 
 
 if __name__ == '__main__':
