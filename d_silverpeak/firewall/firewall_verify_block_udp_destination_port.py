56fbb69762f204553913d017908aba5abb4303c0
Juan Brena <brenajlv@gmail.com>
2020-04-16 15:15:35 -0700

    Add second fw rule ONE to ONE

--- d_silverpeak/firewall/firewall_verify_block_udp_destination_port.py
+++ d_silverpeak/firewall/firewall_verify_block_udp_destination_port.py
@@ -1,5 +1,6 @@
 from my_silverpeak.base_edge import SPBaseEdge
 import json
+import time
 
 """
 Silver Peak Test Plan v2
@@ -63,11 +64,53 @@ def add_firewall_rule_block_udp_destination_port():
                               }
                             }
 
+    deny_destination_port_5060_rule2 = {
+                                  "self": "12_12",
+                                  "prio": {
+                                    "1500": {
+                                      "match": {
+                                        "acl": "",
+                                        "dst_port": "5060"
+                                      },
+                                      "self": 1500,
+                                      "misc": {
+                                        "rule": "enable",
+                                        "logging": "disable",
+                                        "logging_priority": "0",
+                                        "tag": "iTest"
+                                      },
+                                      "comment": "iTest deny UDP traffic by destination port 5060",
+                                      "gms_marked": False,
+                                      "set": {
+                                        "action": "deny"
+                                      }
+                                    },
+                                    "65535": {
+                                      "match": {
+                                        "acl": ""
+                                      },
+                                      "self": 65535,
+                                      "misc": {
+                                        "rule": "enable",
+                                        "logging": "disable"
+                                      },
+                                      "comment": "",
+                                      "gms_marked": False,
+                                      "set": {
+                                        "action": "deny"
+                                      }
+                                    }
+                                  }
+                                }
+
     # Get current firewall rules AKA security policies to Silverpeak
     security_policy_rules = EDGE.api.get_sec_policy(applianceID=EDGE.edge_id).data
 
     # Add rule to security_policy_rules in map One to Default with priority 1500
     security_policy_rules['map1']['12_0']['prio']['1500'] = deny_destination_port_5060_rule
+
+    # Add to zones 'ONE to ONE' (12_12)
+    security_policy_rules['map1']['12_12'] = deny_destination_port_5060_rule2
 
     # Setup Data for API call
     data = {"data": security_policy_rules, "options": {"merge": False, "templateApply": False}}
@@ -79,6 +122,8 @@ def add_firewall_rule_block_udp_destination_port():
     # Check results
     if result.status_code == 204:
         print({'error': None, 'rows': 1})
+        time.sleep(10)
+        EDGE.reset_port_flow(port=5060)
     else:
         print({'error': result.error, 'rows': 0})
 
@@ -100,6 +145,13 @@ def remove_firewall_rule_block_udp_destination_port():
         print({'error': None, 'rows': 0})
         return
 
+    try:
+        del security_policy_rules['map1']['12_12']
+    except KeyError:
+        # If KeyError then rule does not exist therefore removal successful
+        print({'error': None, 'rows': 0})
+        return
+
     # Setup Data for API call
     data = {"data": security_policy_rules, "options": {"merge": False, "templateApply": False}}
     data = json.dumps(data)
@@ -110,11 +162,13 @@ def remove_firewall_rule_block_udp_destination_port():
     # Check results
     if result.status_code == 204:
         print({'error': None, 'rows': 1})
+        time.sleep(10)
+        EDGE.reset_port_flow(port=5060)
     else:
         print({'error': result.error, 'rows': 0})
 
 
 if __name__ == '__main__':
-    create_edge(edge_id='7.NE', enterprise_id='0', ssh_port="2201")
+    create_edge(edge_id='18.NE', enterprise_id='0', ssh_port="2203")
     # add_firewall_rule_block_udp_destination_port()
     remove_firewall_rule_block_udp_destination_port()
