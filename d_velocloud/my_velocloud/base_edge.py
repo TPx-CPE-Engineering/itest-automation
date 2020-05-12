491bfdbd918982837084105ee460e22a6ff82ee6
Juan Brena <brenajlv@gmail.com>
2020-05-12 13:03:41 -0700

    Add LiveModeAPI

--- d_velocloud/my_velocloud/base_edge.py
+++ d_velocloud/my_velocloud/base_edge.py
@@ -3,6 +3,11 @@ from velocloud.apis import AllApi
 from velocloud.rest import ApiException
 from velocloud.models import EdgeGetEdgeConfigurationStackResultItem, EdgeGetEdgeConfigurationStack, ConfigurationModule
 
+import requests
+import json
+import re
+
+
 from velocloud import configuration
 from requests.packages import urllib3
 urllib3.disable_warnings()
@@ -14,7 +19,7 @@ VC_PASSWORD = '1Maule1!'
 
 
 class BaseEdge:
-    def __init__(self, edge_id: int, enterprise_id: int, ssh_port: int, auto_operator_login=True):
+    def __init__(self, edge_id: int, enterprise_id: int, ssh_port: int, auto_operator_login=True, live_mode=False):
 
         self.client = ApiClient(host=VC_SERVER)
         self.api = AllApi(api_client=self.client)
@@ -34,6 +39,12 @@ class BaseEdge:
         self.configuration_stack = self.get_configuration_stack()
         self.edge_specific_profile: EdgeGetEdgeConfigurationStackResultItem = self.configuration_stack[0]
         self.enterprise_profile: EdgeGetEdgeConfigurationStackResultItem = self.configuration_stack[1]
+
+        # Live Mode API
+        if live_mode:
+            self.LiveMode = LiveModeAPI(api_client=self.client,
+                                        edge_id=self.id,
+                                        enterprise_id=self.enterprise_id)
 
     def refresh_configuration_stack(self):
         """
@@ -167,3 +178,99 @@ class BaseEdge:
         # If no cpe ssh rule found return an empty dict
         print('Traceback: No CPE SSH Port Forwarding Rule found')
         return {}
+
+
+class LiveModeAPI:
+    def __init__(self, api_client, edge_id, enterprise_id):
+        self.id = edge_id
+        self.enterprise_id = enterprise_id
+
+        self.api_client = api_client
+        self._session = requests.Session()
+
+        # To avoid creating a new cookie, reuse client's cookie for request Session
+        cookie_obj = requests.cookies.create_cookie(name='velocloud.session',
+                                                    domain=VC_SERVER,
+                                                    value=api_client.default_headers['Cookie'][18:])
+        self._session.cookies.set_cookie(cookie=cookie_obj)
+
+        self._root_url = self._get_root_url(VC_SERVER)
+        self._portal_url = self._root_url + "/portal/"
+        self._live_pull_url = self._root_url + "/livepull/liveData/"
+        self._verify_ssl = False
+        self._seqno = 0
+        self._token = self._get_token()
+
+    def authenticate(self, is_operator=True):
+        """
+        Authenticate to API - on success, a cookie is stored in the session
+        """
+        path = "/login/operatorLogin" if is_operator else "/login/enterpriseLogin"
+        url = self._root_url + path
+        data = {"username": VC_USERNAME, "password": VC_PASSWORD}
+        headers = {"Content-Type": "application/json"}
+        r = self._session.post(url, headers=headers, data=json.dumps(data),
+                               allow_redirects=False, verify=self._verify_ssl)
+
+    def request(self, method, params, ignore_null_properties=False):
+        """
+        Build and submit a request
+        Returns method result as a Python dictionary
+        """
+        self._seqno += 1
+
+        headers = {"Content-Type": "application/json"}
+        method = self._clean_method_name(method)
+        payload = {"jsonrpc": "2.0",
+                   "id": self._seqno,
+                   "method": method,
+                   "params": params}
+
+        if method == "liveMode/readLiveData" or method == "liveMode/requestLiveActions":
+            url = self._live_pull_url
+        else:
+            url = self._portal_url
+
+        r = self._session.post(url, headers=headers,
+                               data=json.dumps(payload), verify=self._verify_ssl)
+
+        kwargs = {}
+        if ignore_null_properties:
+            kwargs["object_hook"] = self._remove_null_properties
+        response_dict = r.json(**kwargs)
+        if "error" in response_dict:
+            raise ApiException(response_dict["error"]["message"])
+        return response_dict["result"]
+
+    @staticmethod
+    def _remove_null_properties(data):
+        return {k: v for k, v in data.iteritems() if v is not None}
+
+    @staticmethod
+    def _clean_method_name(raw_name):
+        """
+        Ensure method name is properly formatted prior to initiating request
+        """
+        return raw_name.strip("/")
+
+    @staticmethod
+    def _get_root_url(hostname):
+        """
+        Translate VCO hostname to a root url for API calls
+        """
+        if hostname.startswith("http"):
+            re.sub("http(s)?://", "", hostname)
+        proto = "https://"
+        return proto + hostname
+
+    def _get_token(self):
+        """
+        Get Live Mode Token needed to do LiveMode Requests
+        :return: str Token
+        """
+
+        response = self.request(method="liveMode/enterLiveMode", params={'enterpriseId': self.enterprise_id,
+                                                                         'id': self.id})
+
+        return response['token']
+
