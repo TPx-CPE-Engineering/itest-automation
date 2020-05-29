e0fe5b5b459b4ae0a660e468ccf65b2f710c802d
dataeng <cpe-itest@tpx.com>
2020-05-29 11:12:13 -0700

    Finish test case

--- d_velocloud/bgp_routing/bgp_routing_test.py
+++ d_velocloud/bgp_routing/bgp_routing_test.py
@@ -6,6 +6,7 @@ import time
 
 # Hardcoded values for test. Update if need to.
 BGP_SETTINGS = {'Segment Name': 'Global Segment',
+                'Segment ID': 0,
                 'BGP Enabled': True,
                 'Local ASN': '65535',
                 'Neighbor IP': '192.168.144.2',
@@ -14,7 +15,7 @@ BGP_SETTINGS = {'Segment Name': 'Global Segment',
 # Ixia Settings
 # Config File
 # IX_NET_CONFIG_FILE_BASE = 'C:\\Users\\dataeng\\AppData\\Local\\Ixia\\IxNetwork\\AutomationConfigs\\'
-IX_NET_CONFIG_FILE_BASE = '/d_ixia/ix_network/configs\\'
+IX_NET_CONFIG_FILE_BASE = 'C:\\Users\\dataeng\\PycharmProjects\\iTest_Automation\\d_ixia\\ix_network\\configs\\'
 IX_NET_CONFIG_FILE = 'bgp_routing.ixncfg'
 FULL_CONFIG = IX_NET_CONFIG_FILE_BASE + IX_NET_CONFIG_FILE
 
@@ -93,11 +94,12 @@ EDGE: BGPRoutingEdge
 # Objects for Ixia IxNetwork
 SESSION_ASSISTANT: SessionAssistant
 IX_NETWORK: SessionAssistant.Ixnetwork
+PORT_MAP: SessionAssistant.PortMapAssistant
 
 
-def load_and_start_ix_network_config():
+def start_ix_network():
     # Initiate IxNetwork session
-    global SESSION_ASSISTANT, IX_NETWORK
+    global PORT_MAP, SESSION_ASSISTANT, IX_NETWORK
 
     # Initiate Session
     SESSION_ASSISTANT = SessionAssistant(IpAddress='10.255.20.7',
@@ -110,23 +112,23 @@ def load_and_start_ix_network_config():
     # Load Config
     IX_NETWORK.info('Loading config...')
     try:
-        IX_NETWORK.LoadConfig(Files(file_path=FULL_CONFIG, local_file=False))
+        IX_NETWORK.LoadConfig(Files(file_path=FULL_CONFIG, local_file=True))
     except BadRequestError as e:
         print({'error': f"{e.message}"})
         exit(-1)
     IX_NETWORK.info('Config loaded.')
 
-    port_map = SESSION_ASSISTANT.PortMapAssistant()
+    PORT_MAP = SESSION_ASSISTANT.PortMapAssistant()
 
     # Connect every port in PORTS
     for port in PORTS:
-        port_map.Map(IpAddress=port['Chassis IP'],
+        PORT_MAP.Map(IpAddress=port['Chassis IP'],
                      CardId=port['Card'],
                      PortId=port['Port'],
                      Name=port['Name'])
 
     IX_NETWORK.info('Connecting to ports...')
-    port_map.Connect(ForceOwnership=FORCE_OWNERSHIP)
+    PORT_MAP.Connect(ForceOwnership=FORCE_OWNERSHIP)
     IX_NETWORK.info('Ports connected.')
 
     time.sleep(35)
@@ -145,23 +147,21 @@ def load_and_start_ix_network_config():
     IX_NETWORK.info('Checking for BGP Session Up...')
     bgp_aggregated_stats = SESSION_ASSISTANT.StatViewAssistant(ViewName='BGP Aggregated Statistics', Timeout=180)
 
-    while not bgp_aggregated_stats.CheckCondition(ColumnName='Sess. Up',
-                                                  Comparator=StatViewAssistant.EQUAL,
-                                                  ConditionValue=1):
-        IX_NETWORK.info('Waiting for BGP Session Up to equal 1...')
-        time.sleep(10)
+    while True:
+        try:
+            while not bgp_aggregated_stats.CheckCondition(ColumnName='Sess. Up',
+                                                          Comparator=StatViewAssistant.EQUAL,
+                                                          ConditionValue=1):
+                IX_NETWORK.info('Waiting for BGP Session Up to equal 1...')
+                time.sleep(10)
+        except SyntaxError:
+            continue
+        break
 
     IX_NETWORK.info('BGP Session Up.')
 
-    # Apply traffic
-    IX_NETWORK.info('Applying traffic...')
-    IX_NETWORK.Traffic.Apply()
-    IX_NETWORK.Traffic.StartStatelessTrafficBlocking()
-    time.sleep(60)
 
-    # Stop traffic
-    IX_NETWORK.Traffic.StopStatelessTrafficBlocking()
-
+def stop_ix_network():
     # Stop protocols
     IX_NETWORK.info('Stopping protocols...')
     IX_NETWORK.StopAllProtocols()
@@ -169,32 +169,122 @@ def load_and_start_ix_network_config():
 
     # Disconnect PORTS
     IX_NETWORK.info('Disconnecting ports...')
-    port_map.Disconnect()
+    PORT_MAP.Disconnect()
     IX_NETWORK.info('Port disconnected.')
 
-
-def stop_ix_network_config():
-
-    # Connect to Initiated IxNetwork session
-
-    # Stop Test
-    print('todo')
+    # Exit out of EDGE Live Mode gracefully
+    EDGE.LiveMode.exit_live_mode()
 
 
 def check_bgp_settings():
     EDGE.check_bgp_settings()
 
 
+def do_advertise_routes_match(edges_routes):
+    # Variable 'edges_routes' sometimes with subnet mask ex. 4.2.2.2/32. 'edges_routes' comes from VC BGP Neighbor Advertised Function.
+    # We want to remove the subnet mask from the string to make it easier to match.
+    # We will strip the subnet mask from 'edges_routes' and only have the ips.
+    edge_advertise_routes_ips = []
+    for route in edges_routes:
+        edge_advertise_routes_ips.append(route.split('/')[0])
+
+    # # Initiate globals for testing.
+    # # Comment out for iTest run
+    # SESSION_ASSISTANT = SessionAssistant(IpAddress='10.255.20.7',
+    #                                      LogLevel=SessionAssistant.LOGLEVEL_INFO,
+    #                                      ClearConfig=False)
+    #
+    # # Get IxNetwork object from Session
+    # IX_NETWORK = SESSION_ASSISTANT.Ixnetwork
+
+    ipv4_unicast = IX_NETWORK.Vport.find(Name='Single 540 LAN').Protocols.find().Bgp.NeighborRange.find().LearnedInformation.Ipv4Unicast.find()
+
+    # Create list of ips taken from Protocol -> BGP -> 'Single 540 LAN' -> IPv4 Peers -> 'Internal - 192.168.144.2-1' -> Learned Routes
+    ix_network_advertise_routes_ips = []
+    for ip in ipv4_unicast:
+        ix_network_advertise_routes_ips.append(ip.IpPrefix)
+
+    # Check if the ips match, expecting to match to pass the test
+    if edge_advertise_routes_ips == ix_network_advertise_routes_ips:
+        print({'match': 'yes'})
+    else:
+        print({'match': 'no'})
+
+    print({'Edge Advertise Routes IPs': edge_advertise_routes_ips})
+    print({'IxNetwork Advertise Routes IPs': ix_network_advertise_routes_ips})
+
+
+def do_received_routes_match(edges_routes):
+    # Variable 'edges_routes' comes with subnet mask.
+    # Remove subnet mask to make verifying matching easier.
+    try:
+        import ipaddress
+    except ImportError:
+        print('Please install module: ipaddress')
+        exit(-1)
+
+    edge_received_routes_ips = []
+    for route in edges_routes:
+        edge_received_routes_ips.append(route.split('/')[0])
+
+    # # Initiate globals for testing.
+    # # Comment out for iTest run
+    # SESSION_ASSISTANT = SessionAssistant(IpAddress='10.255.20.7',
+    #                                      LogLevel=SessionAssistant.LOGLEVEL_INFO,
+    #                                      ClearConfig=False)
+    #
+    # # Get IxNetwork object from Session
+    # IX_NETWORK = SESSION_ASSISTANT.Ixnetwork
+
+    route_ranges = IX_NETWORK.Vport.find(Name='Single 540 LAN').Protocols.find().Bgp.NeighborRange.find().RouteRange.find()
+
+    ix_network_received_routes_ips = []
+    for route in route_ranges:
+        number_of_routes = route.NumRoutes
+        ip = ipaddress.ip_address(address=route.NetworkAddress)
+        while number_of_routes > 0:
+            ix_network_received_routes_ips.append(str(ip))
+            ip = ip + 256
+            number_of_routes -= 1
+
+    if edge_received_routes_ips == ix_network_received_routes_ips:
+        print({'match': 'yes'})
+    else:
+        print({'match': 'no'})
+
+    print({'Edge Received Routes IPs': edge_received_routes_ips})
+    print({'IxNetwork Received Routes IPs': ix_network_received_routes_ips})
+
+
 def get_bgp_neighbor_received_routes():
-    print(EDGE.LiveMode.get_bgp_neighbor_received_routes(segment_id=0, neighbor_ip='192.168.144.2'))
+    while True:
+        try:
+            print(EDGE.LiveMode.get_bgp_neighbor_received_routes(segment_id=0, neighbor_ip='192.168.144.2'))
+        except TimeoutError:
+            print('error time out')
+            continue
+        break
 
 
 def get_bgp_neighbor_advertised_routes():
-    print(EDGE.LiveMode.get_bgp_neighbor_advertised_routes(segment_id=0, neighbor_ip='192.168.144.2'))
+    while True:
+        try:
+            print(EDGE.LiveMode.get_bgp_neighbor_advertised_routes(segment_id=0, neighbor_ip='192.168.144.2'))
+        except TimeoutError:
+            print('Time out error. Trying again')
+            continue
+        break
 
 
 def get_bgp_summary():
-    print(EDGE.LiveMode.get_bgp_summary())
+    while True:
+        try:
+            print(EDGE.LiveMode.get_bgp_summary())
+        except TimeoutError:
+            print('Time out error. Trying again')
+            time.sleep(10)
+            continue
+        break
 
 
 def create_edge(edge_id, enterprise_id):
@@ -203,7 +293,13 @@ def create_edge(edge_id, enterprise_id):
 
 
 if __name__ == '__main__':
-    # create_edge(edge_id=3, enterprise_id=1)
+    create_edge(edge_id=3, enterprise_id=1)
     # check_bgp_settings()
-    load_and_start_ix_network_config()
-    # new_new()
+    # start_ix_network()
+    # do_advertise_routes_match(edges_routes=['4.2.2.2/32', '10.0.1.0/30', '10.0.2.0/30', '10.0.3.0/30', '10.0.80.0/30', '10.0.81.0/30', '10.0.82.0/30',
+    #                                               '10.0.83.0/30', '10.0.86.0/30', '10.0.87.0/30', '10.0.88.0/30', '10.10.1.0/30', '10.10.32.0/30',
+    #                                               '10.10.84.0/24', '10.33.34.0/30', '66.66.66.66/32', '172.16.33.0/29', '192.168.17.0', '192.168.24.0',
+    #                                               '192.168.44.0', '192.168.53.0', '192.168.84.0', '192.168.99.1/32', '192.168.101.0', '192.168.124.0',
+    #                                               '192.168.144.0', '192.168.164.0', '192.168.184.0', '192.168.221.0', '208.57.7.36/32'])
+    # do_received_routes_match(edges_routes=['4.2.2.2/32'])
+    get_bgp_neighbor_received_routes()
