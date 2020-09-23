7c315d43a03362c54f6dcb78d950d24a62f2aa95
Juan Brena <36947974+juanbrena@users.noreply.github.com>
2020-09-23 15:27:05 -0700

    ED-706 Add route verification routes

--- d_velocloud/bgp/bgp_3_00_verify_neighbor_adjacency_advertised_received_routes.py
+++ d_velocloud/bgp/bgp_3_00_verify_neighbor_adjacency_advertised_received_routes.py
@@ -1,5 +1,7 @@
 from my_velocloud.VelocloudEdge import BGPVeloCloudEdge
 from ix_network.Ix_Network import IxNetwork
+import time
+from ipaddress import ip_address
 
 DUT_EDGE: BGPVeloCloudEdge
 IX_NETWORK: IxNetwork
@@ -33,7 +35,7 @@ def create_edge(edge_id, enterprise_id):
     DUT_EDGE.overwrite_bgp_neighbors(neighbor_ip=new_neighbor_ip, neighbor_asn=new_neighbor_asn)
 
     # Initiate Ix Network
-    IX_NETWORK = IxNetwork()
+    IX_NETWORK = IxNetwork(clear_config=False)
 
 
 def get_bgp_summary():
@@ -54,7 +56,151 @@ def get_bgp_neighbor_received_routes():
                                                     neighbor_ip=neighbor_ip))
 
 
+def get_advertised_routes() -> []:
+    """
+    Get Edges Advertised Routes
+    :return:
+    """
+    segment_id = DUT_EDGE.default_bgp_segment['segment']['segmentId']
+    neighbor_ip = DUT_EDGE.get_new_bgp_neighbor_ip()
+    response = DUT_EDGE.get_bgp_neighbor_advertised_routes(segment_id=segment_id, neighbor_ip=neighbor_ip)
+
+    response_lines = response.splitlines()
+    header_start = 0
+    for item in range(0, len(response_lines)):
+        # Find Header
+        if 'Network' in response_lines[item] and 'Next Hop' in response_lines[item] and 'Metric' in response_lines[item]:
+            header_start = item
+
+    bgp_advertised_routes = []
+    # Get all the bgp entries after the table header
+    for line in response_lines[header_start + 1:]:
+        line_split = line.split()
+
+        if len(line_split) == 7:
+            bgp_entry = {
+                'Status Code': line_split[0],
+                'Network': line_split[1],
+                'Next Hop': line_split[2],
+                'Metric': line_split[3],
+                'LocPrf': line_split[4],
+                'Weight': line_split[5],
+                'Path': line_split[6]
+            }
+            bgp_advertised_routes.append(bgp_entry)
+
+    return bgp_advertised_routes
+
+
+def do_advertise_routes_match():
+    """
+    Prints yes or no if IxNetwork IPv4 Unicast Routes IP's match with Velo BGP Neighbor Advertised Routes IPs
+    :return: none
+    """
+    # Parameter 'edges_routes' comes from VC BGP Neighbor Advertised Function.
+    # It is a list of ip address and sometimes they have the subnet mask ex. 4.2.2.2/32.
+    # We want to remove the subnet mask from the string to make it easier to match.
+    # We will strip the subnet mask from 'edges_routes' and only have the ips.
+    edge_advertise_routes = get_advertised_routes()
+    edge_advertise_routes_ips = []
+    for route in edge_advertise_routes:
+        edge_advertise_routes_ips.append(route['Network'].split('/')[0])
+
+    # Get Neighbor Range
+    neighbor_range = IX_NETWORK.IxNetwork.Vport.find().Protocols.find().Bgp.NeighborRange.find()
+    neighbor_range.RefreshLearnedInfo()
+    time.sleep(10)
+
+    ipv4_unicast = neighbor_range.find().LearnedInformation.Ipv4Unicast.find()
+
+    # Create list of ips taken from Protocol -> BGP
+    # -> DUT Port -> IPv4 Peers -> 'Internal - 192.168.144.2-1' -> Learned Routes
+    ix_network_advertise_routes_ips = []
+    for ip in ipv4_unicast:
+        ix_network_advertise_routes_ips.append(ip.IpPrefix)
+
+    # Check if the ips match, expecting to match to pass the test
+    if edge_advertise_routes_ips == ix_network_advertise_routes_ips:
+        print({'match': 'yes'})
+    else:
+        print({'match': 'no'})
+
+    print({'Edge Advertise Routes IPs': edge_advertise_routes_ips})
+    print({'IxNetwork Advertise Routes IPs': ix_network_advertise_routes_ips})
+
+
+def get_learned_routes() -> []:
+    """
+    Get Edges Learned Routes
+    :return:
+    """
+    segment_id = DUT_EDGE.default_bgp_segment['segment']['segmentId']
+    neighbor_ip = DUT_EDGE.get_new_bgp_neighbor_ip()
+    response = DUT_EDGE.get_bgp_neighbor_received_routes(segment_id=segment_id, neighbor_ip=neighbor_ip)
+
+    response_lines = response.splitlines()
+
+    # Find header start
+    header_start = 0
+    for item in range(0, len(response_lines)):
+        if 'Network' in response_lines[item] and 'Next Hop' in response_lines[item] and 'Metric' in response_lines[item]:
+            header_start = item
+
+    bgp_learned_routes = []
+    # Get all the bgp entries after the table header
+    for line in response_lines[header_start + 1:]:
+        line_split = line.split()
+        if len(line_split) == 6:
+            bgp_entry = {
+                'Status Code': line_split[0],
+                'Network': line_split[1],
+                'Next Hop': line_split[2],
+                'LocPrf': line_split[3],
+                'Weight': line_split[4],
+                'Path': line_split[5]
+            }
+            bgp_learned_routes.append(bgp_entry)
+
+    return bgp_learned_routes
+
+
+def do_received_routes_match():
+    """
+    Prints yes or no if IxNetwork Route Range IPs match with Velo BGP Recieved Routes IPs
+    :return: none
+    """
+    # Parameter 'edges_routes' comes from VC BGP Neighbor Advertised Function.
+    # It is a list of ip address and sometimes they have the subnet mask ex. 4.2.2.2/32.
+    # We want to remove the subnet mask from the string to make it easier to match.
+    # We will strip the subnet mask from 'edges_routes' and only have the ips.
+    edge_received_routes = get_learned_routes()
+    edge_received_routes_ips = [route['Network'].split('/')[0] for route in edge_received_routes]
+
+    neighbor_range = IX_NETWORK.IxNetwork.Vport.find().Protocols.find().Bgp.NeighborRange.find()
+    neighbor_range.RefreshLearnedInfo()
+
+    route_ranges = neighbor_range.RouteRange.find()
+
+    # Gather Ix Network Routes IPs
+    ix_network_received_routes_ips = []
+    for route in route_ranges:
+        number_of_routes = route.NumRoutes
+        ip = ip_address(address=route.NetworkAddress)
+        while number_of_routes > 0:
+            ix_network_received_routes_ips.append(str(ip))
+            # Increase IP by 256
+            ip = ip + 256
+            number_of_routes -= 1
+
+    if edge_received_routes_ips == ix_network_received_routes_ips:
+        print({'match': 'yes'})
+    else:
+        print({'match': 'no'})
+
+    print({'Edge Received Routes IPs': edge_received_routes_ips})
+    print({'IxNetwork Received Routes IPs': ix_network_received_routes_ips})
+
+
 if __name__ == '__main__':
     create_edge(edge_id=4, enterprise_id=1)
-    # start_ixia()
-    get_bgp_neighbor_received_routes()
+    do_received_routes_match()
