51206d27cdc0ef2d6582d21fab91429f502a5c00
Juan Brena <juan.brena@tpx.com>
2020-11-17 11:50:35 -0800

    ED-716 Add LAN Side NAT 5.00 Test Case

--- d_velocloud/lan_side_nat/lsn_5_00_verify_1_to_1_in_dnat_out_snat.py
+++ d_velocloud/lan_side_nat/lsn_5_00_verify_1_to_1_in_dnat_out_snat.py
@@ -1,16 +1,57 @@
 from my_velocloud.VelocloudEdge import LANSideNatVelocloudEdge
+from networking_scripts.my_ssh import ssh_connect
 
 DUT_EDGE: LANSideNatVelocloudEdge
 
+STATIC_ROUTE_SUBNET = '172.16.223.0'
+STATIC_ROUTE_SUBNET_MASK = '24'
+
+
+def add_static_route():
+    """
+    Adds a static route to voice segment
+    :return: None
+    """
+    voice_vlan = DUT_EDGE.get_voice_segment_vlan()
+    voice_vlan_ip_address = voice_vlan['cidrIp']
+
+    static_route_rule = {
+        "destination": STATIC_ROUTE_SUBNET,
+        "netmask": "255.255.255.0",
+        "sourceIp": None,
+        "gateway": voice_vlan_ip_address,
+        "cost": 0,
+        "preferred": True,
+        "description": "Added by iTest",
+        "cidrPrefix": STATIC_ROUTE_SUBNET_MASK,
+        "wanInterface": "",
+        "subinterfaceId": -1,
+        "icmpProbeLogicalId": None,
+        "vlanId": None,
+        "advertise": True
+    }
+
+    print(DUT_EDGE.add_static_route_rule_to_segment(segment_name='Voice', rule=static_route_rule))
+
 
 def add_lan_side_nat_rule():
+    """
+    Add LAN-Side NAT Rule
+    :return: nONE
+    """
+
+    inside_address = DUT_EDGE.get_cpe_lan_ip()
+    inside_address_mask = 32
+
+    outside_address = '172.16.223.20'
+    outside_address_mask = 32
 
     nat_rule = {
-        "insideCidrIp": "10.0.0.0",
-        "insideCidrPrefix": 32,
+        "insideCidrIp": inside_address,
+        "insideCidrPrefix": inside_address_mask,
         "insidePort": -1,
-        "outsideCidrIp": "192.168.0.0",
-        "outsideCidrPrefix": 24,
+        "outsideCidrIp": outside_address,
+        "outsideCidrPrefix": outside_address_mask,
         "outsidePort": -1,
         "type": "source",
         "description": "Added by iTest",
@@ -31,11 +72,17 @@ def delete_all_nat_rules():
     print(DUT_EDGE.delete_all_nat_rules_from_segment(segment_name='Voice'))
 
 
-def create_edge(edge_id, enterprise_id):
+def create_edge(edge_id, enterprise_id, cpe_ssh_port):
     global DUT_EDGE
-    DUT_EDGE = LANSideNatVelocloudEdge(edge_id=edge_id, enterprise_id=enterprise_id)
+    DUT_EDGE = LANSideNatVelocloudEdge(edge_id=edge_id, enterprise_id=enterprise_id, cpe_ssh_port=cpe_ssh_port)
+
+    print(DUT_EDGE.disable_advertise_on_vlan(vlan_name='Voice'))
+
+    add_static_route()
 
 
 if __name__ == '__main__':
-    create_edge(edge_id=245, enterprise_id=1)
-    delete_all_nat_rules()
+    create_edge(edge_id=245, enterprise_id=1, cpe_ssh_port=2202)
+    # ssh_client = ssh_connect(host='10.255.20.159', port=22, username='cpeeng', password='Fan-Brain-K')
+    # stdin, stdout, stderr = ssh_client.exec_command(command='ping -c 5 8.8.8.8')
+    # print(stdout.read().decode('ascii'))
