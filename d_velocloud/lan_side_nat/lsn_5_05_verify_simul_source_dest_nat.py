6340a764a98c0a21a27a44adad190f9056d3cff1
Juan Brena <juan.brena@tpx.com>
2021-02-02 14:36:44 -0800

    ED-716 Add functions for 5.05 - still not done

---
+++ d_velocloud/lan_side_nat/lsn_5_05_verify_simul_source_dest_nat.py
@@ -0,0 +1,117 @@
+from my_velocloud.VelocloudEdge import LANSideNatVelocloudEdge
+
+"""
+Verify simultaneous source & destination NAT
+
+Usage:
+a.	Configure a Source & Destination LAN-side NAT at site 1: source mapping is 192.168.135.155/32 -> 172.16.135.155/32; 
+destination mapping is 172.16.238.155/32 -> 192.168.138.155/32.
+b.	Do not advertise VLAN 1 at site 1.
+c.	Verify that host 1 can ping host 2 with the NAT IP 172.16.238.155.
+d.	Verify that host 2 can ping host 1 with the NAT IP 172.16.135.155.
+
+Expected Results:
+Host 1 able to ping host 2 with NAT IP 172.16.238.155
+Host 2 able to ping host 1 with NAT IP 172.16.135.155
+"""
+
+DUT_EDGE: LANSideNatVelocloudEdge
+
+STATIC_ROUTE_SUBNET = '172.16.223.0'
+STATIC_ROUTE_SUBNET_MASK = '24'
+
+
+def add_static_route():
+    """
+    Adds a static route to voice segment
+    :return: None
+    """
+
+    """
+    Adding 1 Static Route Rule
+
+    Subnet                  Source IP   Next Hop                   Interface           VLAN    Cost Preferred  Advertise
+    [STATIC ROUTE SUBNET]   n/a         [VOICE VLAN IP ADDRESS]    [not applicable]    none    0    true       true
+    """
+
+    voice_vlan = DUT_EDGE.get_voice_segment_vlan()
+    voice_vlan_ip_address = voice_vlan['cidrIp']
+
+    static_route_rule = {
+        "destination": STATIC_ROUTE_SUBNET,
+        "netmask": "255.255.255.0",
+        "sourceIp": None,
+        "gateway": voice_vlan_ip_address,
+        "cost": 0,
+        "preferred": True,
+        "description": "Added by iTest",
+        "cidrPrefix": STATIC_ROUTE_SUBNET_MASK,
+        "wanInterface": "",
+        "subinterfaceId": -1,
+        "icmpProbeLogicalId": None,
+        "vlanId": None,
+        "advertise": True
+    }
+
+    print("Adding a Static Route on Voice Segment...")
+    print(DUT_EDGE.add_static_route_rule_to_segment(segment_name='Voice', rule=static_route_rule))
+
+
+def add_lan_side_nat_rule():
+    """
+    Add LAN-Side NAT Rule
+    :return: None
+    """
+
+    """
+    Adding LAN-Side NAT Rule -> NAT Source and Destination
+
+    Type    Inside Addr     Outside Addr    Type    Inside Addr     Outside Addr
+    Source  172.16.223.21   
+    """
+
+    # NAT RULE
+    nat_rule = {
+          "srcInsideCidrIp": "192.168.135.155",
+          "srcOutsideCidrIp": "172.16.135.155",
+          "destInsideCidrIp": "172.16.238.155",
+          "destOutsideCidrIp": "192.168.138.155",
+          "srcInsideCidrPrefix": 32,
+          "srcOutsideCidrPrefix": 32,
+          "destInsideCidrPrefix": 32,
+          "destOutsideCidrPrefix": 32,
+          "description": "Added by iTestverify",
+          "srcInsideNetmask": "255.255.255.255",
+          "srcOutsideNetmask": "255.255.255.255",
+          "destInsideNetmask": "255.255.255.255",
+          "destOutsideNetmask": "255.255.255.255"
+        }
+
+    print("Adding LAN-Side NAT Rules on Voice Segment...")
+    print(DUT_EDGE.add_nat_rules_to_segment(segment_name='Voice', rules=[], dual_rules=[nat_rule]))
+
+
+def create_edge(edge_id, enterprise_id, cpe_ssh_port):
+    global DUT_EDGE
+    DUT_EDGE = LANSideNatVelocloudEdge(edge_id=edge_id, enterprise_id=enterprise_id, cpe_ssh_port=cpe_ssh_port)
+
+    add_lan_side_nat_rule()
+    # print("Setting Voice VLAN Advertise Enabled to False...")
+    # print(DUT_EDGE.set_advertise_on_vlan(advertise_enabled=False, vlan_name='Voice'))
+
+    # add_static_route()
+
+
+def restore_configuration():
+    print("Setting Voice VLAN Advertise Enabled to True...")
+    print(DUT_EDGE.set_advertise_on_vlan(advertise_enabled=True, vlan_name='Voice'))
+
+    print("Deleting all Static Routes in Voice Segment...")
+    print(DUT_EDGE.delete_all_static_routes_from_segment(segment_name='Voice'))
+
+    print("Deleting all LAN-NAT Rules in Voice Segment...")
+    print(DUT_EDGE.delete_all_nat_rules_from_segment(segment_name='Voice'))
+
+
+if __name__ == '__main__':
+    create_edge(edge_id=249, enterprise_id=1, cpe_ssh_port=2202)
