ecc8e8c729c3d3126ed94c0185fd4b22b221a1c4
Juan Brena <juan.brena@tpx.com>
2021-02-05 11:06:55 -0800

    ED-716 Correct comments

--- d_velocloud/lan_side_nat/lsn_5_06_verify_source_nat_route_matching.py
+++ d_velocloud/lan_side_nat/lsn_5_06_verify_source_nat_route_matching.py
@@ -4,16 +4,16 @@ from my_velocloud.VelocloudEdge import LANSideNatVelocloudEdge
 Verify Source NAT route matching
 
 Usage:
-a.	Configure a Source LAN-side NAT at site 1: inside 192.168.135.155/32, outside 172.16.135.155/32, 
-destination route 192.168.138.155/32.
+a.	Configure a Source LAN-side NAT at site 1: inside [CPE IP]/32, outside 172.16.223.21/32, 
+destination route 192.168.100.100/32.
 b.	Do not advertise VLAN 1 at site 1.
-c.	Verify that host 1 can ping host 2 at 192.168.138.155.
+c.	Verify that host 1 can ping host 2 at 192.168.100.100.
 d.	Modify the LAN-side NAT destination route to be 172.31.0.11/32.
-e.	Verify that host 1 can no longer ping host 2 at 192.168.138.155.
+e.	Verify that host 1 can no longer ping host 2 at 192.168.100.100.
 
 Expected Results:
-Before step (d) verify host 1 is able to ping host 2 at 192.168.138.155
-After step (d) verify  host 1 is no longer able to ping host 2 at 192.168.138.155
+Before step (d) verify host 1 is able to ping host 2 at 192.168.100.100
+After step (d) verify  host 1 is no longer able to ping host 2 at 192.168.100.100
 """
 
 DUT_EDGE: LANSideNatVelocloudEdge
@@ -25,14 +25,12 @@ STATIC_ROUTE_SUBNET_MASK = '24'
 def add_static_route():
     """
     Adds a static route to voice segment
-    :return: None
-    """
 
-    """
-    Adding 1 Static Route Rule
-
+    Static Route Settings
     Subnet                  Source IP   Next Hop                   Interface           VLAN    Cost Preferred  Advertise
-    [STATIC ROUTE SUBNET]   n/a         [VOICE VLAN IP ADDRESS]    [not applicable]    none    0    true       true
+    [STATIC_ROUTE_SUBNET]   n/a         [VOICE VLAN IP ADDRESS]    [not applicable]    none    0    true       true
+
+    :return: None
     """
 
     voice_vlan = DUT_EDGE.get_voice_segment_vlan()
@@ -60,15 +58,13 @@ def add_static_route():
 
 def add_cpe_ip_lan_side_nat_rule():
     """
-    Add LAN-Side NAT Rule with CPE IP as inside addr
+    Add LAN-Side NAT Rule with CPE IP as Inside Addr
+
+    NAT Source or Destination
+    Type          Inside Addr       Outside Addr           Destination Route
+    Source        [CPE IP]/32       172.16.223.21/32       192.168.100.100/32
+
     :return: None
-    """
-
-    """
-    Adding LAN-Side NAT Rule -> NAT Source and Destination
-
-    Type    Inside Addr     Outside Addr    Type    Inside Addr     Outside Addr
-    Source  172.16.223.21   
     """
 
     cpe_ip = DUT_EDGE.get_cpe_lan_ip()
@@ -100,14 +96,12 @@ def add_cpe_ip_lan_side_nat_rule():
 def add_random_ip_lan_side_nat_rule():
     """
     Add LAN-Side NAT Rule
+
+    NAT Source or Destination
+    Type          Inside Addr       Outside Addr           Destination Route
+    Source        172.31.0.11/32    172.16.223.21/32       192.168.100.100/32
+
     :return: None
-    """
-
-    """
-    Adding LAN-Side NAT Rule -> NAT Source and Destination
-
-    Type    Inside Addr     Outside Addr    Type    Inside Addr     Outside Addr
-    Source  172.16.223.21   
     """
 
     # NAT RULE
