352b43df507a42866e8640c07c5174485df0e170
Juan Brena <juan.brena@tpx.com>
2021-06-16 11:12:20 -0700

    add business policy class for Business Policy test cases

---
+++ d_velocloud/business_policy/business_policy_class.py
@@ -0,0 +1,8 @@
+from my_velocloud.VelocloudEdge import VeloCloudEdge
+
+
+class BPVeloCloudEdge(VeloCloudEdge):
+    def __init__(self, edge_id, enterprise_id):
+        super().__init__(edge_id, enterprise_id)
+
+    # Any related to BP test case/s you can add it here
