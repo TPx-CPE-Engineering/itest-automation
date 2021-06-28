7b6dcfbaf4548b09992dff28ff02114a92df1af6
chill_TPx <cody.hill@tpx.com>
2021-06-28 10:51:39 -0700

    Minor updates

--- d_velocloud/business_policy/business_policy_1_01_verify_data_sent_out_correct_wan.py
+++ d_velocloud/business_policy/business_policy_1_01_verify_data_sent_out_correct_wan.py
@@ -11,18 +11,16 @@
 # (Configure > Edges > Business Policy > New Rule)
 #
 # Steps:
-# 1.) Create Edge
-# 2.) Get active WAN interfaces
-# 3.) Start IxLoad
-# 4.) Set Business Policy to prefer WAN1
-# 5.) Flush Flows
-# 6.) List active flows and verify for WAN1
-# 7.) Remove Business policy preferring WAN1
-# 8.) Set Business Policy to prefer WAN2
-# 9.) Flush Flows
-# 10.) List active flows and verify for WAN2
-# 11.) Remove Business Policy preferring WAN2
-# 12.) Clean Up
+# 1.)  Get VeloCloud Edge
+# 2.)  Get active Edge WAN interfaces
+# 3.)  Configure Business Policy to prefer one interface over the other
+# 4.)  Flush all active flows
+# 5.)  Begin FTP transfer in ixLoad
+# 6.)  List active flows from source IP and confirm that data traffic is flowing through matching Business Policy
+# 7.)  Re-configure Business Policy to prefer the other WAN interface
+# 8.)  Flush all active flows
+# 9.)  List active flows from source IP and confirm that data traffic is flowing through matching Business Policy
+# 10.) Clean up
 
 
 from my_velocloud.VelocloudEdge import VeloCloudEdge
@@ -63,7 +61,7 @@ def main():
 
         print('\nConnecting to IxLoad')
         # Enable IxLoad for FTP throughput testing
-        ix_load = IxLoadApi()
+        ix_load = IxLoadApi(api_server_ip="10.255.20.196")
         ix_load.connect(ixLoadVersion=ix_load.ixLoadVersion)
         print('\nLoading IxLoad config file')
         ix_load.loadConfigFile(rxfFile="C:\\Users\\dataeng\\Documents\\Ixia\\IxLoad\\Repository\\Dev_VeloSingle3400+FTP2Ixia.rxf")
@@ -71,18 +69,18 @@ def main():
         ix_load.enableForceOwnership()
         print('\nEnabling IxLoad analyzer')
         ix_load.enableAnalyzerOnAssignedPorts()
-        print('\nStarting IxLoad')
+        print('\nSetting up IxLoad and pressing go')
         ix_load.runTraffic()
 
         statsDict = {
-            'FTPClient': [{'caption': 'Throughput (Kbps)', 'operator': '>', 'expect': 0}]
+            'restStatViews': [{'caption': 'FTP Client - Throughput Objective', 'operator': '>', 'expect': 0}]
         }
 
         ix_load.pollStatsAndCheckStatResults(statsDict=statsDict)
 
         # TODO: List flows to verify traffic is being sent over correct interface
-        print('\nTraffic completed for 5 minutes. Sleeping for 30 seconds')
-        time.sleep(30)
+        print('\nIxLoad is running traffic. Sleeping for 2 minutes.')
+        time.sleep(120)
 
         # Remove Business Policy rule that prefers interface
         edge.remove_business_policy_rule_from_preferred_interface(
@@ -95,3 +93,103 @@ def main():
 if __name__ == '__main__':
     edge = create_edge(edge_id=240, enterprise_id=1)
     main()
+
+    # Leaving for later
+    # rule = {
+    #     "name": "[AUTOMATION] Prefer GE3",
+    #     "match": {
+    #         "appid": -1,
+    #         "classid": -1,
+    #         "dscp": -1,
+    #         "sip": "any",
+    #         "sport_high": -1,
+    #         "sport_low": -1,
+    #         "ssm": "255.255.255.255",
+    #         "svlan": -1,
+    #         "os_version": -1,
+    #         "hostname": "",
+    #         "dip": "any",
+    #         "dport_low": -1,
+    #         "dport_high": -1,
+    #         "dsm": "255.255.255.255",
+    #         "dvlan": -1,
+    #         "proto": -1,
+    #         "s_rule_type": "prefix",
+    #         "d_rule_type": "prefix"
+    #     },
+    #     "action": {
+    #         "routeType": "edge2Any",
+    #         "allowConditionalBh": False,
+    #         "userDisableConditionalBh": False,
+    #         "edge2EdgeRouteAction": {
+    #             "interface": "GE3",
+    #             "subinterfaceId": -1,
+    #             "linkInternalLogicalId": "auto",
+    #             "linkPolicy": "fixed",
+    #             "routeCfg": {},
+    #             "routePolicy": "gateway",
+    #             "serviceGroup": "ALL",
+    #             "vlanId": -1,
+    #             "wanlink": "GE3",
+    #             "linkCosLogicalId": None,
+    #             "linkOuterDscpTag": "CS0",
+    #             "linkInnerDscpTag": None
+    #         },
+    #         "edge2DataCenterRouteAction": {
+    #             "interface": "GE3",
+    #             "subinterfaceId": -1,
+    #             "linkInternalLogicalId": "auto",
+    #             "linkPolicy": "fixed",
+    #             "routeCfg": {},
+    #             "routePolicy": "auto",
+    #             "serviceGroup": "ALL",
+    #             "vlanId": -1,
+    #             "wanlink": "GE3",
+    #             "linkCosLogicalId": None,
+    #             "linkOuterDscpTag": "CS0",
+    #             "linkInnerDscpTag": None
+    #         },
+    #         "edge2CloudRouteAction": {
+    #             "interface": "GE3",
+    #             "subinterfaceId": -1,
+    #             "linkInternalLogicalId": "auto",
+    #             "linkPolicy": "fixed",
+    #             "routeCfg": {},
+    #             "routePolicy": "gateway",
+    #             "serviceGroup": "ALL",
+    #             "vlanId": -1,
+    #             "wanlink": "GE3",
+    #             "linkCosLogicalId": None,
+    #             "linkOuterDscpTag": "CS0",
+    #             "linkInnerDscpTag": None
+    #         },
+    #         "QoS": {
+    #             "type": "transactional",
+    #             "rxScheduler": {
+    #                 "bandwidth": -1,
+    #                 "bandwidthCapPct": -1,
+    #                 "queueLen": -1,
+    #                 "burst": -1,
+    #                 "latency": -1,
+    #                 "priority": "normal"
+    #             },
+    #             "txScheduler": {
+    #                 "bandwidth": -1,
+    #                 "bandwidthCapPct": -1,
+    #                 "queueLen": -1,
+    #                 "burst": -1,
+    #                 "latency": -1,
+    #                 "priority": "normal"
+    #             }
+    #         },
+    #         "sla": {
+    #             "latencyMs": "0",
+    #             "lossPct": "0.0",
+    #             "jitterMs": "0"
+    #         },
+    #         "nat": {
+    #             "sourceIp": "no",
+    #             "destIp": "no"
+    #         }
+    #     }
+    # }
