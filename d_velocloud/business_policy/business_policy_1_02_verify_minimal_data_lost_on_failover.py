49a8f9835284077ac5e60dc40fbb7aeca807be96
Juan Brena <juan.brena@tpx.com>
2021-09-15 09:44:25 -0700

    add python file for test case

---
+++ d_velocloud/business_policy/business_policy_1_02_verify_minimal_data_lost_on_failover.py
@@ -0,0 +1,10 @@
+from business_policy_class import BPVeloCloudEdge as BPVeloCloudEdge_
+
+
+class BPVeloCloudEdge(BPVeloCloudEdge_):
+    def __init__(self, edge_id, enterprise_id):
+        super().__init__(edge_id, enterprise_id)
+
+
+if __name__ == '__main__':
+    pass
