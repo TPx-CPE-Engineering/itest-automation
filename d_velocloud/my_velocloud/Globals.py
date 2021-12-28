9d259b398e17fc7e1290f8a230b68d66a4eb0a69
iTest Automation <cpeengineering@tpx.com>
2021-12-28 11:28:47 -0800

    update credentials

--- d_velocloud/my_velocloud/Globals.py
+++ d_velocloud/my_velocloud/Globals.py
@@ -1,8 +1,8 @@
 
 # Velocloud Login Credentials
 VC_SERVER = 'cpevc.lab-sv.telepacific.com'
-VC_USERNAME = 'juan.brena@tpx.com'
-VC_PASSWORD = '1Maule1!'
+VC_USERNAME = 'jbrena@tpx.com'
+VC_PASSWORD = 'Ifixit2021!'
 
 # Velocloud 'Voice' Segment
 VOICE_SEGMENT_NAME = 'Voice'
