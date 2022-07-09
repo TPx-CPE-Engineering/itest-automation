be7d1fadc5b8945021bdaca9103dcf6dd8f615f6
Cody Hill <cody.hill@tpx.com>
2022-07-08 17:28:10 -0700

    Cleaned up code

--- d_polycom/poly_1_01_call_cancel_dut_to_pstn.py
+++ d_polycom/poly_1_01_call_cancel_dut_to_pstn.py
@@ -13,6 +13,7 @@ Scenario:
 """
 
 import time
+import json
 
 
 def originate_call_from_dut_to_pstn(dut_poly: object, pstn_poly_1: object):
@@ -21,19 +22,12 @@ def originate_call_from_dut_to_pstn(dut_poly: object, pstn_poly_1: object):
     Args:
         dut_poly (object): DUT Poly object
         pstn_poly_1 (object): PSTN Poly object
-
-    Returns:
-        True (tuple): If the call was successfully sent from the DUT Poly
-        False (tuple): If the call was unsuccessful from the DUT Poly
     """
 
     result = dut_poly.web_call_control_dial(pstn_poly_1.phone_number)
 
-    if not result:
-        return False, result[1]
-
     time.sleep(5)
-    return True, result[1]
+    print(json.dumps(result))
 
 
 def check_if_called_party_is_ringing(pstn_poly_1: object):
@@ -41,18 +35,11 @@ def check_if_called_party_is_ringing(pstn_poly_1: object):
 
     Args:
         pstn_poly_1 (object): PSTN Poly 1 object
-
-    Returns:
-        False (tuple): If the called party did not receive Ringing
-        True (tuple): If the called party successfully received Ringing
     """
 
     result = pstn_poly_1.is_ringing()
 
-    if not result:
-        return False, result[1]
-
-    return True, result[1]
+    print(json.dumps(result))
 
 
 def check_if_originating_party_receives_ringback(dut_poly: object):
@@ -60,18 +47,11 @@ def check_if_originating_party_receives_ringback(dut_poly: object):
 
     Args:
         dut_poly (object): DUT Poly object
-
-    Returns:
-        False (tuple): If the originating party did not receive RingBack
-        True (tuple): If the originating party successfully receives RingBack
     """
 
     result = dut_poly.check_for_ringback()
 
-    if not result:
-        return False, result[1]
-
-    return True, result[1]
+    print(json.dumps(result))
 
 
 def release_call_from_dut(dut_poly: object):
@@ -79,23 +59,13 @@ def release_call_from_dut(dut_poly: object):
 
     Args:
         dut_poly (object): DUT Poly object
-
-    Returns:
-        False (tuple): If the call was not released from the DUT Poly
-        True (tuple): If the call was successfully released from the DUT Poly
     """
 
-    call_reference = dut_poly.get_current_call_reference()
+    call_reference = dut_poly.get_current_call_reference()['call_reference']
 
-    if not call_reference:
-        return False, call_reference[1]
+    result = dut_poly.web_call_control_end_call(call_reference)
 
-    result = dut_poly.web_call_control_end_call(call_reference[1])
-
-    if not result:
-        return False, result[1]
-
-    return True, result[1]
+    print(json.dumps(result))
 
 
 if __name__ == '__main__':
