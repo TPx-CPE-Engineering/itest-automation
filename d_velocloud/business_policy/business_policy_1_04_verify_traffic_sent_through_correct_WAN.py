1c9acd6921eeaf8f6221652194b9b03c77ed1bee
Juan Brena <brenajlv@gmail.com>
2023-01-17 14:23:52 -0800

    add methods to test case & update requirements.txt

---
+++ d_velocloud/business_policy/business_policy_1_04_verify_traffic_sent_through_correct_WAN.py
@@ -0,0 +1,18 @@
+from business_policy_class import BPVeloCloudEdge
+import json
+
+
+def get_public_interfaces():
+    pass
+
+
+def did_icmp_traffic_increase(interface):
+    pass
+
+
+def main():
+    edge = BPVeloCloudEdge(edge_id=17, enterprise_id=1)
+
+
+if __name__ == '__main__':
+    main()
--- d_velocloud/business_policy/business_policy_class.py
+++ d_velocloud/business_policy/business_policy_class.py
@@ -1,6 +1,12 @@
 from my_velocloud.VelocloudEdge import VeloCloudEdge
 from ipaddress import ip_address
+from datetime import datetime
 import json
+import time
+import pytz     # TODO install in CPE python venv
+import pandas as pd     # TODO install in CPE python venv
+import pandas.core.frame
+from typing import Optional
 
 
 class BPVeloCloudEdge(VeloCloudEdge):
@@ -14,6 +20,61 @@ class BPVeloCloudEdge(VeloCloudEdge):
             link['isLinkIpAddressPublic'] = not ip_address(link['publicIpAddress']).is_private
         return json.dumps(wan_settings['data']['links'])
 
+    @staticmethod
+    def convert_to_datetime(timestamp: int, timezone='UTC'):
+        """
+        Converts a given Datto timestamp or a Epoch Unix milliseconds timestamp into a datetime
+        :param timestamp: Int, Epoch Unix milliseconds timestamp
+        :param timezone: Str, pytz timezone, default UTC
+        :return: Obj, datetime
+        """
+        return datetime.fromtimestamp(timestamp / 1000, tz=pytz.timezone(timezone))
+
+    def get_live_icmp_transport_metrics(self, max_seconds) -> Optional[pandas.core.frame.DataFrame]:
+
+        # enter live mode and set live mode token
+        self.set_live_mode_token()
+
+        all_data = []
+
+        seconds_to_collect_data = 0
+        # print(f"Start time: {datetime.now(tz=pytz.timezone('America/Los_Angeles')).strftime('%Y-%m-%d %H:%M:%S')}")
+        while seconds_to_collect_data < max_seconds:
+            seconds_to_collect_data += 1
+            response = self.client.call_api(method='liveMode/readLiveData', params={'token': self.live_mode_token})
+
+            if response['status']['isActive']:
+                if response['data']:
+                    link_data = response['data']['linkStats']['data']
+
+                    for link in link_data:
+                        ts_datetime = self.convert_to_datetime(timestamp=link['timestamp'], timezone='America/Los_Angeles')
+
+                        if link['data']:
+                            for entry in link['data']:
+                                icmpBytesRx = entry['icmpBytesRx']
+                                icmpBytesTx = entry['icmpBytesTx']
+                                name = entry['name']
+                                d = {
+                                    'Time': ts_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                                    'Name': name,
+                                    'ICMP Bytes Rx': icmpBytesRx,
+                                    'ICMP Bytes Tx': icmpBytesTx
+                                }
+                                all_data.append(d)
+            time.sleep(1)
+
+        self.exit_live_mode()
+
+        if len(all_data) != 0:
+            df2 = pd.DataFrame(all_data)
+            print(df2.to_string())
+            return df2
+        else:
+            print({'error': 'No metrics to show'})
+            return None
+
 
 if __name__ == '__main__':
-    edge = BPVeloCloudEdge(edge_id=245, enterprise_id=1)
+    edge = BPVeloCloudEdge(edge_id=10, enterprise_id=1)
+    edge.get_live_icmp_transport_metrics(max_seconds=180)
